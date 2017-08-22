d0eedd69d30b4b3871bd379240ff9b6f3fe71e7b
raian <you@example.com>
2017-08-22 00:40:32 -0400

    first commit

--- SimpleUISearchBar/ViewController.swift
+++ SimpleUISearchBar/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tblSearch: UITableView!
+    
+    var isSearch : Bool = false
+    var arrCountry = ["Afghanistan", "Algeria", "Bahrain","Brazil", "Cuba", "Denmark","Denmark", "Georgia", "Hong Kong", "Iceland", "India", "Japan", "Kuwait", "Nepal", "MEXICO", "CANADA", "ESTADOS UNIDOS", "VENEZUELA", "COLOMBIA"];
+    var arrFilter:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tblSearch.dataSource = self
+        self.tblSearch.delegate = self
+        self.searchBar.delegate = self
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrCountry.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell 	{
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = self.arrCountry[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row \(indexPath.row) selected")
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar){
+        print("SHIT")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        print(searchText)
+        let index = IndexPath(row: 2, section: 0) // use your index number or Indexpath
 
-
+        tblSearch.scrollToRow(at: index, at: .middle, animated: true)
+    }
 }
 
